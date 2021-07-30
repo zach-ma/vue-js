481a8c4acc8d92f719ab8afb6230643d93b1670c
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-07-29 20:51:12 -0400

    finished section 5: vue behind the scenes

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,62 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.log(this.$refs.userText);
+      // console.dir(this.$refs.userText);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate()");
+  },
+  created() {
+    console.log("created()");
+  },
+  beforeMount() {
+    console.log("beforeMount()");
+  },
+  mounted() {
+    console.log("mounted()");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount()");
+  },
+  unmounted() {
+    console.log("unmounted()");
+  },
+});
+
+app.mount("#app");
+
+setTimeout(() => {
+  app.unmount();
+}, 3000);
+
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ favourateMeal }}</p>
+  `,
+  data() {
+    return {
+      favourateMeal: "Pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
