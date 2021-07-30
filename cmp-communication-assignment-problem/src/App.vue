5d5932dd66d40a83b0bd8eeaa30c3be900f09128
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-07-30 18:28:04 -0400

    finished assignment 6 props and custom events

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <active-user :username="user.name" :userage="user.age"></active-user>
+  <user-data @set-data="setUserData"></user-data>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Zach",
+        age: 20,
+      },
+    };
+  },
+  methods: {
+    setUserData(userName, userAge) {
+      this.user = {
+        name: userName,
+        age: +userAge,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h2>User Name: {{ username }}</h2>
+    <h3>User Age: {{ userage }}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  // NOTE
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userage: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
