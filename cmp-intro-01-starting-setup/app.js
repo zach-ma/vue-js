b751f561746ee72a107bccf1b76cdfa1bd8c0401
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-07-30 01:43:49 -0400

    finished section 6: intro to components

---
+++ cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,56 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "Julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+});
+
+// create component
+// NOTE custom html tag with -, use multi-word
+app.component("friend-contact", {
+  template: `
+    <li :key="friend.id">
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">
+            {{detailsAreVisible ? 'Hide' : 'Show'}} Details
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+        </ul>
+    </li>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+
+app.mount("#app");
