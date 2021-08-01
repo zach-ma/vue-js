e503499f155a689a3c2baadc782a244c6071b262
MJX-Git <63674024+MJX-Git@users.noreply.github.com>
2021-08-01 02:16:42 -0400

    finished section 10: project, the learning resources app

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <ul></ul>
+  <the-header title="Remember Me"></the-header>
+  <!-- <stored-resources :resources="storedResources"></stored-resources> -->
+  <the-resources></the-resources>
 </template>
 
 <script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
 export default {
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google ...',
-          link: 'https://google.com'
-        }
-      ]
-    };
-  }
+  components: { TheHeader, TheResources }
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Roboto', sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
